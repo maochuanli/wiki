a357ad06da595c04d33695d7a66b0edff21f7186
Mao Chuan Li <mao.chuan.li@gmail.com>
2020-04-17 10:03:57 +1200

    Update emacs.init.el

--- emacs.init.el
+++ emacs.init.el
@@ -21,3 +21,12 @@
     "Revert buffer without confirmation."
     (interactive)
     (revert-buffer :ignore-auto :noconfirm))
+
+;; Set up backup folder
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
